8b3c247d45a277ee6187bebe14b8c37d6116ab4d
Stanislaw Baranski <stachu@stasbar.com>
2019-10-31 16:49:05 +0100

    add facebook and google auth received token WIP

--- backend/main.go
+++ backend/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"encoding/json"
-  "encoding/base64"
+  "time"
 	"fmt"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/julienschmidt/httprouter"
+ _ "github.com/dgrijalva/jwt-go"
 	"io/ioutil"
+  "io"
 	"log"
 	"net/http"
 	"net/url"
@@ -101,6 +103,23 @@ func deltaQuantity(w http.ResponseWriter, r *http.Request, ps httprouter.Params)
 	//TODO
 }
 
+func authFacebook(fbAppAccessToken string) httprouter.Handle{
+  return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    bodyJson, err := toJson(r.Body)
+    if err != nil {
+      log.Println(err)
+    }
+    accessToken := bodyJson["accessToken"]
+    uri := "https://graph.facebook.com/debug_token?input_token="+accessToken+"&access_token="+fbAppAccessToken
+    debugToken,err :=  getJsonMap(uri)
+    if err != nil {
+      log.Println(err)
+    }
+    log.Println(debugToken)
+    // TODO test
+  }
+}
+
 func authGoogle(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
   body, err := ioutil.ReadAll(r.Body)
   if err != nil {
@@ -117,17 +136,95 @@ func authGoogle(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
     fmt.Printf(" %s = %v\n", key, value)
   }
 
-  decoded, err := base64.StdEncoding.DecodeString(params["tokenId"][0])
-  if err != nil {
-    fmt.Fprint(w, "Error", err)
-  }
-  fmt.Fprint(w, decoded)
+  jwtToken := params["tokenId"][0]
+  url := "https://oauth2.googleapis.com/tokeninfo?id_token="+jwtToken
+  getJsonMap(url)
+  // TODO 
+  // debug with 
+
+  // 1. Verify signature based on getGoogleJwtClaims()
+  // jwt.Parse(jwtToken, )
+  // 2. check if iss is https://accounts.google.com or accounts.google.com
+  // 3. ckeck of aud is app client id
+  // 4. exp is not past
+  // get userinfo_endpoint from discovery
+  // https://accounts.google.com/.well-known/openid-configuration
+
+
+
   // TODO exchange code for access token and ID Token. 
 }
 
-func authFacebook(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-
+func getGoogleJwtClaims() {
+  // get JSON from https://accounts.google.com/.well-known/openid-configuration
+  jsonMap, err := getJsonMap("https://accounts.google.com/.well-known/openid-configuration")
+  if err != err {
+    log.Fatal(err)
+    return
+  }
+  log.Println(jsonMap)
+  log.Println(jsonMap["jwks_uri"])
 }
+
+func getFacebookAppAccessToken() (string, error){
+	clientId := os.Getenv("WAREHOURSER_CLIENT_ID")
+	clientSecret := os.Getenv("WAREHOURSER_CLIENT_SECRET")
+  jsonMap, err := getJsonMap("https://graph.facebook.com/oauth/access_token?client_id="+clientId+"&client_secret="+clientSecret+"&grant_type=client_credentials")
+  if err != nil {
+    log.Fatal(err)
+    return "", err
+  }
+  return jsonMap["access_token"], nil
+}
+
+func toJson(r io.ReadCloser) (map[string]string, error) {
+  body, readErr := ioutil.ReadAll(r)
+  if readErr != nil {
+    log.Println(readErr)
+    return nil, readErr
+  }
+  jsonMap := map[string]string{}
+  jsonErr := json.Unmarshal(body, &jsonMap)
+  if jsonErr != nil {
+    return nil, jsonErr
+  }
+  return jsonMap, nil
+}
+
+func getJsonMap(url string) (map[string]string, error) {
+  jsonMap := map[string]string{}
+  return jsonMap, getJson(url, &jsonMap)
+}
+
+func getJson(url string, outObj interface{}) error {
+  client := http.Client{
+    Timeout: time.Second * 2,
+  }
+  req, err := http.NewRequest(http.MethodGet, url, nil)
+  if err != nil {
+    log.Println(err)
+    return err
+  }
+  req.Header.Set("User-Agent", "warehouser-resource")
+
+  res, getErr := client.Do(req)
+  if getErr != nil {
+    log.Println(getErr)
+    return getErr
+  }
+
+  body, readErr := ioutil.ReadAll(res.Body)
+  if readErr != nil {
+    log.Println(readErr)
+    return readErr
+  }
+  jsonErr := json.Unmarshal(body, outObj)
+  if jsonErr != nil {
+    return jsonErr
+  }
+  return nil
+}
+
 
 func logger(next httprouter.Handle) httprouter.Handle {
   return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
@@ -137,6 +234,10 @@ func logger(next httprouter.Handle) httprouter.Handle {
 }
 
 func main() {
+  fbAppAccessToken, err := getFacebookAppAccessToken()
+  if err != nil {
+    log.Fatal(err)
+  }
 	router := httprouter.New()
 	router.GET("/products", logger(getAllProducts))
 	router.GET("/products/:id", logger(getProduct))
@@ -144,7 +245,7 @@ func main() {
 	router.DELETE("/products/:id", logger(deleteProduct))
 	router.PATCH("/deltaQuantity/:id", logger(deltaQuantity))
   router.POST("/auth/google", logger(authGoogle))
-  router.POST("/auth/facebook", logger(authFacebook))
+  router.POST("/auth/facebook", logger(authFacebook(fbAppAccessToken)))
 	sslCert := os.Getenv("STASBAR_SSL_CERT")
 	sslKey := os.Getenv("STASBAR_SSL_KEY")
 	httpsPort := os.Getenv("HTTPS_PORT")
