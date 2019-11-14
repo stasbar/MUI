233aadcc638821cf9bb6f286bbe5c6abbddc85ed
Stanislaw Baranski <stachu@stasbar.com>
2019-11-15 00:58:40 +0100

    extends session time

--- backend/consent/routes/login.js
+++ backend/consent/routes/login.js
@@ -78,7 +78,7 @@ router.post('/', csrfProtection, function (req, res, next) {
     remember: Boolean(req.body.remember),
 
     // When the session expires, in seconds. Set this to 0 so it will never expire.
-    remember_for: 3600,
+    remember_for: 60 * 60 * 12, // 12 hours
 
     // Sets which "level" (e.g. 2-factor authentication) of authentication the user has. The value is really arbitrary
     // and optional. In the context of OpenID Connect, a value of 0 indicates the lowest authorization level.
